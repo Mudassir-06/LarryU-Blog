6df8e90d81240b9b8c12dcb2a16a5eccd44ed0f1
Hamza Ehsan <hamza.ehsan@ninjatrader.com>
2022-03-22 08:36:52 -0500

    secrets configured

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 class Config:
     # "import secrets" in Terminal -> secrets.token_hex(16).
-    SECRET_KEY = "c188044384c87340b6589cd20cf75491"
-    SQLALCHEMY_DATABASE_URI  = "sqlite:///site.db"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI  =  os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
-    MAIL_SERVER ='smtp.gmail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = 587
     MAIL_USE_TLS =True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
