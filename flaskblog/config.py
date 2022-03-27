18ed5dfaacaaa19799815d0d8e98c7be4546e6df
Mudassir Zaeem <mudassirzaeem@Mudassirs-MacBook-Pro.local>
2022-03-27 17:15:41 -0500

    DB_URL

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     # "import secrets" in Terminal -> secrets.token_hex(16).
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI  =  os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI  =  os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = 587
