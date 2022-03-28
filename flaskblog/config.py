cb38197ce13b6c634cdfc3354dacc46289c42a62
Mudassir Zaeem <mudassirzaeem@Mudassirs-MacBook-Pro.local>
2022-03-27 21:49:28 -0500

    URL again

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,9 +7,9 @@ load_dotenv()
 class Config:
     # "import secrets" in Terminal -> secrets.token_hex(16).
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI  =  os.environ.get('SQLALCHEMY_DATABASE_URI')
-    # if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    #     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI  =  os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = 587
