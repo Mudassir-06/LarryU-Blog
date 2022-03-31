445192853b71b626b9c56deed7341f8846063595
Mudassir Zaeem <mudassirzaeem@Mudassirs-MacBook-Pro.local>
2022-03-31 12:16:08 -0500

    message

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -3,7 +3,7 @@ from flaskblog.models import Post
 
 main = Blueprint('main', __name__)
 
-# Setting up an main.route decorator will handle the complicated backend stuff and allow us to write a function that returns the information that will be shown on the site
+# Setting up a main.route decorator will handle the complicated backend stuff and allow us to write a function that returns the information that will be shown on the site
 # Think of it as a root page
 @main.route("/")
 @main.route("/home")
